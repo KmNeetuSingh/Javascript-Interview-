58e3462f888c3e8e85154bd44ef78db1d61ac113
Neetu Singh <neetuisngh99590@gmail.com>
2025-02-10 22:37:07 +0530

    is done

---
+++ Day-1/index2.js
@@ -0,0 +1,14 @@
+// function fact(N){
+//     let r = 1
+//  for (let i = 1; i <= N; i++){
+//      r *= i
+//  }
+//  return r
+// }
+// console.log(fact(7))
+// So if we  started to use recursive ....
+function fact(N){
+    if(N === 1 || N === 0){
+        return 1
+    }
+}

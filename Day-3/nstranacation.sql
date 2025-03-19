45a009742a86fa912f422880b729883fef34c3dc
KmNeetuSingh <itisneetusingh@gmail.com>
2025-03-19 21:56:11 +0530

    Practice

--- Day-3/nstranacation.sql
+++ Day-3/nstranacation.sql
@@ -24,6 +24,47 @@ commit
 commit
 -- once completed data can't be change or lost
 
+Start transaction
+update account set balance = balance + 1000 where id = 1
+update account set balance = balance - 1000 where id = 2
+rollback
+-- Atomicity
+Start transaction 
+update account set balance = balance + 1000 where id = 1
+where balance >= 1000;
+commit
+-- Consistency
+Start transaction
+Select * from account where id = 1 for update;
+update account set balance = balance - 500 where id = 1;
+update account set balance = balance - 500 where id = 2;
+commit
+-- Durability
+commit
+-- once completed data can't be change or lost
 
 
+-- Acid
+-- Atomicity
+-- Consistency
+-- Isolation
+-- Durability
+-- /Atomicity
+Start transaction
+update account set balance = balance + 1000 where id = 1
+update account set balance = balance - 1000 where id = 2
+rollback
 
+-- Consistency
+Start transaction
+update account set balance = balance + 1000 where id = 1
+where balance >= 1000;
+commit
+-- Isolation
+Start transaction
+Select * from account where id = 1 for update;
+update account set balance = balance - 500 where id = 1;
+update account set balance = balance - 500 where id = 2;
+commit
+-- Durability
+commit
